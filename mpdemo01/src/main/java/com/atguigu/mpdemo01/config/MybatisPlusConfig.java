25a2e116090a82421d850db0d9bb039cf4083344
Hushunkang <2752962035@qq.com>
2020-04-04 19:17:54 +0800

    谷粒学院 mp入门 分页查询

--- mpdemo01/src/main/java/com/atguigu/mpdemo01/config/MybatisPlusConfig.java
+++ mpdemo01/src/main/java/com/atguigu/mpdemo01/config/MybatisPlusConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.mpdemo01.config;
 
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,12 @@ public class MybatisPlusConfig {
     @Bean
     public OptimisticLockerInterceptor optimisticLockerInterceptor() {
         return new OptimisticLockerInterceptor();
+    }
+
+    //mybatis plus分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
     }
 
 }
--- mpdemo01/src/test/java/com/atguigu/mpdemo01/Mpdemo01ApplicationTests.java
+++ mpdemo01/src/test/java/com/atguigu/mpdemo01/Mpdemo01ApplicationTests.java
@@ -2,11 +2,15 @@ package com.atguigu.mpdemo01;
 
 import com.atguigu.mpdemo01.entity.User;
 import com.atguigu.mpdemo01.mapper.UserMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class Mpdemo01ApplicationTests {
@@ -65,6 +69,46 @@ class Mpdemo01ApplicationTests {
         user.setEmail("helen@atguigu.com");
         //执行更新
         userMapper.updateById(user);
+    }
+
+    //多个id批量查询
+    @Test
+    public void testSelectBatchIds(){
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1L, 2L, 3L, 4L, 5L));
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    //多条件查询
+    @Test
+    public void testSelectByMap(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","Jone");
+        map.put("age",18);
+        List<User> users = userMapper.selectByMap(map);
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    //分页查询
+    @Test
+    public void testSelectPage(){
+        //创建Page对象，传入两个参数：当前页和每页显示的记录数
+        Page<User> page = new Page<>(1,3);
+        //调用mp分页查询过程中，底层封装，把分页所有数据封装到page对象里面
+        userMapper.selectPage(page,null);
+
+        //通过page对象获取分页数据
+        System.out.println(page.getCurrent());//当前页
+        System.out.println(page.getSize());//每页显示记录数
+        System.out.println(page.getRecords());//每页数据list集合
+        System.out.println(page.getPages()); //总页数
+        System.out.println(page.getTotal()); //总记录数
+
+        System.out.println(page.hasPrevious()); //有上一页吗
+        System.out.println(page.hasNext()); //有下一页吗
     }
 
 }
