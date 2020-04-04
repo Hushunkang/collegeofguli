5e93150e61de13b851c1ffc9f1752894f03e8474
Hushunkang <2752962035@qq.com>
2020-04-04 14:16:20 +0800

    谷粒学院 mp入门

---
+++ mpdemo01/src/main/java/com/atguigu/mpdemo01/Mpdemo01Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.mpdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mpdemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mpdemo01Application.class, args);
+    }
+
+}
